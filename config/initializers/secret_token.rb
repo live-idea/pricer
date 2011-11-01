757d6f305f722cfc69be3cfd497d2160d3045172
Oleksandr Bondar <obondar@gmail.com>
2011-11-01 18:22:21 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pricer::Application.config.secret_token = '61136c65565382bb9a786c58c99a032e4d4a371c747dfb6f7592dcab059b8915861fe747e360dba0d014dce41fe8923118cded85f723026d8e7683fd6010a2c0'
