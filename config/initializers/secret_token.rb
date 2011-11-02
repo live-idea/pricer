605812953775621920cf459362ea36ff97b051b5
Lp Fan <misha.shchetinin@gmail.com>
2011-11-02 22:51:39 +0200

    start new project. Misha

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pricer::Application.config.secret_token = '53cfe5bef33e04d00e9717431f3f203a87dfd5b9d607d9efa8334cc32d033c9e00e13590438d99897154ddc74334a8781f04d24627452d64615311bac1d23b96'
